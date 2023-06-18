a745df3b3a115b8892f13bb7f8dcd5ec5451dd0f
lucasfads <lucasfads@gmail.com>
2023-06-18 23:43:31 +0000

    installs wp and creates admin; creates user

--- srcs/wordpress/conf/wp-config-create.sh
+++ srcs/wordpress/conf/wp-config-create.sh
@@ -8,4 +8,7 @@ if [ ! -f "/var/www/wp-config.php" ]; then
   /usr/local/bin/wp config set WP_REDIS_TIMEOUT 1
   /usr/local/bin/wp config set WP_REDIS_READ_TIMEOUT 1
   /usr/local/bin/wp config set WP_REDIS_DATABASE 0
+  
+  /usr/local/bin/wp core install --url="https://${INTRA_LOGIN}.42.fr" --title="${WP_SITE_NAME}" --admin_user="${WP_ADMIN_NAME}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}"
+  /usr/local/bin/wp user create "${WP_USER_NAME}" "${WP_USER_EMAIL}" --role="editor" --user_pass="${WP_USER_PASSWORD}"
 fi
